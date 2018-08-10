388630ed8273ff43710713b3486ee20509eb82d3
chane81 <chane81@naver.com>
2018-08-10 10:48:47 +0900

    async SetState test

---
+++ src/components/lesson6/AsyncSetStateTest.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+// 비동기 SetState 호출
+class AsyncSetStateTest extends Component {
+    state = {
+        counter: 0
+    }
+
+    handlerClick = async (e) => {
+
+        await this.setState({
+            counter: this.state.counter + 1
+        });
+
+        await this.setState({
+            counter: this.state.counter + 1
+        });
+
+        alert(this.state.counter);
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handlerClick}>SetState 테스트</button>
+            </div>
+        );
+    }
+}
+
+export default AsyncSetStateTest;
--- src/pages/Lesson6.js
+++ src/pages/Lesson6.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import ComponentIterationSample from 'components/lesson6/IterationSample';
+import AsyncSetStateTest from 'components/lesson6/AsyncSetStateTest';
 
 class Lesson6 extends Component {
     render() {
         return (
-            <ComponentIterationSample />  
+            <div>
+                <p><ComponentIterationSample /></p>
+                <p><AsyncSetStateTest /></p>
+            </div>
         );
     }
 }
