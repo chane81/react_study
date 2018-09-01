c715188cc3f2aa8a106c29ecb7d5b9f219b369a9
chane81 <chane81@naver.com>
2018-09-01 10:44:03 +0900

    Revert "TodoList 의 TodoItem done prop 수정"
    This reverts commit f5604cfa74f93bc3dad2b0de43e026b90057d8c4.

--- src/components/lesson10/TodoList.js
+++ src/components/lesson10/TodoList.js
@@ -5,7 +5,7 @@ export default class TodoList extends Component {
 	render() {
 		return (
 			<div>
-				<TodoItem done={true}>리액트 공부하기</TodoItem>
+				<TodoItem done>리액트 공부하기</TodoItem>
 				<TodoItem>컴포넌트 스타일링 해보기</TodoItem>
 			</div>
 		);
