13c767ffc99a8f8f1b3d8dfa02c20f33e6ffc610
chane81 <chane81@naver.com>
2018-09-03 20:34:18 +0900

    Lesson13  주석수정

--- src/pages/Lesson13.js
+++ src/pages/Lesson13.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import * as actions from "../actions/ActionTypes";
-import CounterList from "../components/lesson13/CounterList";
-import Buttons from "../components/lesson13/Buttons";
-import getRandomColor from "../lib/getRandomColor";
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/ActionTypes';
+import CounterList from '../components/lesson13/CounterList';
+import Buttons from '../components/lesson13/Buttons';
+import getRandomColor from '../lib/getRandomColor';
 
 /* 카운터리스트 Connect */
 // state 를 컴포넌트 props 와 연결
@@ -28,7 +28,7 @@ const ConnectCounter = connect(
 )(CounterList);
 /* 카운터리스트 Connect */
 
-/* 버튼 디스패치, 커넥트 */
+/* 버튼 Connect */
 // action dispatch 를 컴포넌트 props 와 연결
 const mapDispatchButtons = dispatch => ({
 	onCreate: () => dispatch(actions.create(getRandomColor())),
@@ -40,7 +40,7 @@ const ConnectButtons = connect(
 	null,
 	mapDispatchButtons
 )(Buttons);
-/* 버튼 디스패치, 커넥트 */
+/* 버튼 Connect */
 
 export default () => {
 	return (
--- src/pages/Lesson14.js
+++ src/pages/Lesson14.js
@@ -1,6 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { default as MapTest } from '../components/lesson14/MapTest';
+import TodoInput from '../components/lesson10/TodoInput';
+import TodoList from '../components/lesson10/TodoList';
+import * as InputActions from '../modules/lesson14/Input';
+import * as TodosActions from '../modules/lesson14/Todos';
+import { bindActionCreators } from 'redux';
 
-export default () => {
-	return <MapTest />;
+/* TodoInput Connect */
+class TodoInputContainer extends Component {
+	id = 1;
+	getId = () => {
+		return ++this.id;
+	};
+
+	handleChange = e => {
+		const { value } = e.target;
+		const { InputActions } = this.props;
+		InputActions.setInput(value);
+	};
+
+	handleInsert = () => {
+		const { InputActions, TodosActions, value } = this.props;
+		const todo = {
+			id: this.getId(),
+			text: value,
+			done: false
+		};
+
+		TodosActions.insert(todo);
+		InputActions.setInput('');
+	};
+
+	render() {
+		const { value } = this.props;
+
+		return (
+			<TodoInput
+				onChange={this.handleChange}
+				onInsert={this.handleInsert}
+				value={value}
+			/>
+		);
+	}
+}
+
+TodoInputContainer.propTypes = {
+	value: PropTypes.string,
+	InputActions: PropTypes.func,
+	TodosActions: PropTypes.func
 };
+
+export const ConnectTodoInput = connect(
+	state => ({
+		value: state.input.get('value')
+	}),
+	dispatch => ({
+		InputActions: bindActionCreators(InputActions, dispatch),
+		TodosActions: bindActionCreators(TodosActions, dispatch)
+	})
+)(TodoInputContainer);
+/* TodoInput Connect */
+
+/* TodoList Connect */
+class TodoListContainer extends Component {
+	handleToggle = (id) => {
+		const { TodosActions } = this.props;
+	}
+
+	render() {
+		return (
+			<TodoList todos={} onToggle={} onRemove={}></TodoList>
+		);
+	}
+}
+
+TodoListContainer.propTypes = {
+	todos: PropTypes.object,
+	TodosActions: PropTypes.func
+};
+
+export const ConnectTodoList = connect(
+	state => ({
+		todos: state.todos
+	}),
+	dispatch => ({
+		TodosActions: bindActionCreators(TodosActions, dispatch)
+	})
+)(TodoListContainer);
+/* TodoList Connect */
