38c05c7b269b3f43871f0de0b4e6e87a6db2c9db
chane81 <chane81@naver.com>
2018-09-03 10:42:09 +0900

    TodoList.js 테스트

--- src/components/lesson10/TodoList.js
+++ src/components/lesson10/TodoList.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
+	state = {
+		done: false
+	};
+
+	handlerToggle = e => {
+		this.setState({ done: !this.state.done });
+	};
+
 	render() {
 		return (
 			<div>
-				<TodoItem done>리액트 공부하기</TodoItem>
+				<TodoItem done={this.state.done} onToggle={this.handlerToggle}>
+					리액트 공부하기
+				</TodoItem>
 				<TodoItem>컴포넌트 스타일링 해보기</TodoItem>
 			</div>
 		);
--- src/components/lesson5/ScrollBox.js
+++ src/components/lesson5/ScrollBox.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import './ScrollBox.css'
+import './ScrollBox.css';
 
 class ScrollBox extends Component {
-    // 맨밑으로 스크롤내리기
-    scrollToBottom = () => {
-        const { scrollHeight, clientHeight } = this.box;
+	// 맨밑으로 스크롤내리기
+	scrollToBottom = () => {
+		const { scrollHeight, clientHeight } = this.box;
 
-        this.box.scrollTop = scrollHeight - clientHeight;
-    }
+		this.box.scrollTop = scrollHeight - clientHeight;
+	};
 
-    render() {
-        return (
-            <div className='box' ref={(ref) => this.box=ref}>
-                <div className='boxInner' />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="box" ref={ref => (this.box = ref)}>
+				<div className="boxInner" />
+			</div>
+		);
+	}
 }
 
 export default ScrollBox;
