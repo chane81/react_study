fc14149f0c4fefd1fd7a2d9240df42f93f8ab3d5
chane81 <chane81@naver.com>
2018-09-05 14:24:13 +0900

    root.js 수정

--- src/client/Root.js
+++ src/client/Root.js
@@ -7,15 +7,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 // 리듀서 가져오기
-//import reduceReducers from 'reduce-reducers';
 import { combineReducers } from 'redux';
-//import { default as Lesson13Reducers } from '../reducers/index';
+import { default as Lesson13Reducers } from '../reducers/index';
 import { default as Lesson14Reducers } from '../modules/index';
 
-//const reducers = reduceReducers(Lesson14Reducers);
-
-//const rootReducers = reduceReducers({ Lesson14Input, Lesson14Todos });
-const rootReducers = combineReducers({ Lesson14Reducers });
+// 루트 리듀서들
+const rootReducers = combineReducers({ Lesson13Reducers, Lesson14Reducers });
 
 // 스토어 생성
 // 크롬 리덕스 dev tools 를 쓰기위해 구문추가 => window.devToolsExtension && window.devToolsExtension()
