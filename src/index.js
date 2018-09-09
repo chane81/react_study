b3571af600670914adf67a18bb594f6f3ff6ac85
chane81 <chane81@naver.com>
2018-09-09 14:19:06 +0900

    인풋박스에서 엔터키를 눌렀을 때 onInsert 이벤트 핸들러를 props 로 받게 수정

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import Root from './client/Root';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+	<Root base={process.env.PUBLIC_URL || ''} />,
+	document.getElementById('root')
+);
 registerServiceWorker();
