360d8f227f88884aa5519bd35ed26cf52ee2c3b4
chane81 <chane81@naver.com>
2018-09-11 01:16:12 +0900

    logger middleware commit

--- src/client/Root.js
+++ src/client/Root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import App from '../shared/App';
 
 // 리덕스 관련 불러오기
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 // 리듀서 가져오기
@@ -10,14 +10,21 @@ import { combineReducers } from 'redux';
 import { default as Lesson13Reducers } from '../reducers/index';
 import { default as Lesson14Reducers } from '../modules/index';
 
+// 로거 미들웨어 불러오기
+import LoggerMiddleware from '../components/lesson15/LoggerMiddleware';
+import { createLogger } from 'redux-logger';
+
 // 루트 리듀서들
 const rootReducers = combineReducers({ Lesson13Reducers, Lesson14Reducers });
+
+//
 
 // 스토어 생성
 // 크롬 리덕스 dev tools 를 쓰기위해 구문추가 => window.devToolsExtension && window.devToolsExtension()
 const store = createStore(
 	rootReducers,
-	window.devToolsExtension && window.devToolsExtension()
+	window.devToolsExtension && window.devToolsExtension(),
+	applyMiddleware(createLogger())
 );
 
 const Root = ({ baseUrl }) => {
--- src/components/lesson5/ValidationSample.js
+++ src/components/lesson5/ValidationSample.js
@@ -1,42 +1,49 @@
 import React, { Component } from 'react';
-import './ValidationSample.css'
+import './ValidationSample.css';
 
 class ValidationSample extends Component {
-    state = {
-        password: '',
-        clicked: false,
-        validated: false
-    }
+	state = {
+		password: '',
+		clicked: false,
+		validated: false
+	};
 
-    handleChange = (e) => {
-        this.setState({
-            password: e.target.value
-        });
-    }
+	handleChange = e => {
+		this.setState({
+			password: e.target.value
+		});
+	};
 
-    handleButtonClick = (e) => {
-        this.setState({
-            clicked: true,
-            validated: this.state.password === '0000'
-        });
+	handleButtonClick = e => {
+		this.setState({
+			clicked: true,
+			validated: this.state.password === '0000'
+		});
 
-        this.input.focus();
-    }
+		this.input.focus();
+	};
 
-    render() {
-        return (
-            <div>
-                <input
-                    ref={(ref) => this.input=ref}
-                    type='password'
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                    className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
-                />
-                <button onClick={this.handleButtonClick}>검증하기</button>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<input
+					ref={ref => (this.input = ref)}
+					type="password"
+					value={this.state.password}
+					onChange={this.handleChange}
+					className={
+						this.state.clicked
+							? this.state.validated
+								? 'success'
+								: 'failure'
+							: ''
+					}
+				/>
+				<label>{this.state.validated ? '비번맞음' : '비번틀림'}</label>
+				<button onClick={this.handleButtonClick}>검증하기</button>
+			</div>
+		);
+	}
 }
 
 export default ValidationSample;
