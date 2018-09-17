e83c37beedfcc52ae515f054e2f76ffd0d2b333f
chane81 <chane81@naver.com>
2018-09-17 16:54:11 +0900

    TodoList 와 MapTest 컴포넌트의 위치를 수정함

--- lesson_flow/src/pages/Lesson14.jsx
+++ lesson_flow/src/pages/Lesson14.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import PageTemplate from '../components/lesson10/PageTemplate';
-import { default as ContainerTodoInput } from '../Containers/lesson14/TodoInput';
-import { default as ContainerTodoList } from '../Containers/lesson14/TodoList';
-import MapTest from '../components/lesson14/MapTest';
+import React from "react";
+import PageTemplate from "../components/lesson10/PageTemplate";
+import { default as ContainerTodoInput } from "../Containers/lesson14/TodoInput";
+import { default as ContainerTodoList } from "../Containers/lesson14/TodoList";
+import MapTest from "../components/lesson14/MapTest";
 
 export default () => {
 	return (
 		<div>
-			<MapTest />
 			<PageTemplate>
 				<ContainerTodoInput />
 				<ContainerTodoList />
 			</PageTemplate>
+			<MapTest />
 		</div>
 	);
 };
