dd35c10783fa82026fb9c96e0aacfabd39f88b45
chane81 <chane81@naver.com>
2018-09-23 00:58:51 +0900

    link, navlink 사용

--- lesson16_router/src/App.jsx
+++ lesson16_router/src/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Home, About } from './pages/Index';
+import Menu from './components/Menu';
 class App extends Component {
 	render() {
 		return (
 			<div>
+				<Menu />
+
+				{/* 라우트 설정 */}
 				<Route exact path="/" component={Home} />
-				<Route exact path="/about" component={About} />
-				<Route path="/about/:name" component={About} />
+				<Route exact path="/about/:name?" component={About} />
 			</div>
 		);
 	}
