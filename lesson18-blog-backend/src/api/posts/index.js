38cf65915f5539318bea1c195de791e2df45b7c7
chane81 <chane81@naver.com>
2018-10-07 20:52:21 +0900

    lesson18 일부 수정 + lesson19 몽고db 관련 프로젝트 커밋

--- lesson18-blog-backend/src/api/posts/index.js
+++ lesson18-blog-backend/src/api/posts/index.js
@@ -3,13 +3,13 @@ const postsCtrl = require('./posts.ctrl');
 
 const posts = new Router();
 
-const printInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params
-  };
-};
+// const printInfo = (ctx) => {
+//   ctx.body = {
+//     method: ctx.method,
+//     path: ctx.path,
+//     params: ctx.params
+//   };
+// };
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
