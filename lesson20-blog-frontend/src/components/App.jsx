1b21888a6169eb33812facc35cee5d359ff1c079
chane81 <chane81@naver.com>
2018-10-12 02:11:58 +0900

    lesson20 리듀서 모듈 작성시작

---
+++ lesson20-blog-frontend/src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>BlogApp</div>;
+
+export default App;
---
+++ lesson20-blog-frontend/src/store/modules/index.js
@@ -0,0 +1,5 @@
+export { default as editor } from './editor';
+export { default as list } from './editor';
+export { default as post } from './editor';
+export { default as base } from './editor';
+export { penderReducer as pender } from 'redux-pender';
