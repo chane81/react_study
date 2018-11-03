9ce06f72fb04d784d71ed533cfa4cddd61b0739d
chane81 <chane81@naver.com>
2018-11-03 18:19:39 +0900

    mobx-state-tree 소스 커밋

--- lesson99_mobx/src/App.js
+++ lesson99_mobx/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import SuperMarket from './components/SuperMarket.jsx';
+import SuperMarket from './components/SuperMarket';
 import DevTools from 'mobx-react-devtools';
 import PostMobXpromise from './components/Post.jsx';
-import PostMobXpromiseV2 from './components/PostV2.jsx';
+import PostMobXpromiseV2 from './components/PostV2';
+import PostMobXpromiseV3 from './components/PostV3';
 
 class App extends Component {
 	render() {
@@ -22,6 +23,11 @@ class App extends Component {
 								MobX Promise V2
 							</Link>
 						</li>
+						<li>
+							<Link to="/post-mobx-promise-v3">
+								MobX State tree - Promise V3
+							</Link>
+						</li>
 					</ul>
 
 					<Route exact path="/" component={SuperMarket} />
@@ -32,6 +38,10 @@ class App extends Component {
 					<Route
 						path="/post-mobx-promise-v2/"
 						component={PostMobXpromiseV2}
+					/>
+					<Route
+						path="/post-mobx-promise-v3/"
+						component={PostMobXpromiseV3}
 					/>
 
 					{process.env.NODE_ENV === 'development' && <DevTools />}
--- lesson99_mobx/src/index.js
+++ lesson99_mobx/src/index.js
@@ -4,9 +4,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'mobx-react';
 import MarketStore from './stores/MarketStore';
-//import PostStore from './stores/PostStore.jsx';
-import PostStore from './stores/PostMobxPromiseStore.jsx';
-import PostStoreV2 from './stores/PostMobxPromiseStoreV2.jsx';
+//import PostStore from './stores/PostStore';
+import PostStore from './stores/PostMobxPromiseStore';
+import PostStoreV2 from './stores/PostMobxPromiseStoreV2';
+import PostStoreV3 from './stores/PostMobxPromiseStoreV3';
+
 
 const marketStore = new MarketStore();
 const postStore = new PostStore();
@@ -16,7 +18,8 @@ ReactDOM.render(
 	<Provider
 		marketStore={marketStore}
 		postStore={postStore}
-		postStoreV2={postStoreV2}>
+		postStoreV2={postStoreV2}
+		postStoreV3={PostStoreV3}>
 		<App />
 	</Provider>,
 	document.getElementById('root')
